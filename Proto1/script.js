c3806d7f12c3376b6c1f4fc8d9c649cf1b13a66c
TarunwithSha <tarunsaha5827@gmail.com>
2024-01-01 11:43:01 +0530

    Added all files for now

---
+++ Proto1/script.js
@@ -0,0 +1,2 @@
+// No JavaScript needed for the current example
+// You can add JavaScript code here for more functionality in the future
--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # GDSC
 This is my first GDSC git repo. <br>
-Year:2023-24
+Year:2023-24 .
